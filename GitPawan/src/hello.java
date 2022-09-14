1d5adb87aa3be386e6f869d02c00ec1b6d10ce8f
ADMIN <ADMIN@DESKTOP-RJ8F2SK>
2022-09-14 18:02:42 +0530

    First Commit One

---
+++ GitPawan/src/hello.java
@@ -0,0 +1,11 @@
+
+public class hello {
+
+	public static void main(String[] args) {
+	
+		System.out.println("StatementOne");
+		System.out.println("Statementtwo");
+		System.out.println("StatementThree");
+	}
+
+}
